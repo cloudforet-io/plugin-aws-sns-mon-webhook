a285c679523465576713865036086e2a75fa51f4
Jihyung Song <bluese@megazone.com>
2021-08-26 22:36:36 +0900

    refactoring

--- src/spaceone/monitoring/error/event.py
+++ src/spaceone/monitoring/error/event.py
@@ -2,3 +2,11 @@ from spaceone.core.error import *
 
 class ERROR_NOT_TITLE(ERROR_INVALID_ARGUMENT):
     _message = 'Title for Event message from AWS SNS is Missing (event = {event})'
+
+
+class ERROR_PARSE_EVENT(ERROR_BASE):
+    _message = 'Failed to parse event (field)'
+
+
+class ERROR_GET_JSON_MESSAGE(ERROR_BASE):
+    _message = 'Failed to get json (raw_json)'
